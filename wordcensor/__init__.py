e79941b4467a2e35811450f014df799acdfebfd9
R3v1L <R3v1L@users.noreply.github.com>
2015-02-18 13:36:03 +0000

    Changed dependency from evodjango to django-sitetools

--- wordcensor/__init__.py
+++ wordcensor/__init__.py
@@ -9,8 +9,8 @@ Django word censoring application
 .. moduleauthor:: (C) 2014 Oliver Gutiérrez
 """
 
-# EVODjango imports
-from evodjango.utils import inject_app_defaults
+# Site tools imports
+from sitetools.utils import inject_app_defaults
 
 # Default application settings injection
 inject_app_defaults(__name__)
